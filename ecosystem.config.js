7acf025857978dbf1841ed5d572a84a9fe517d7b
nainish <pmo_dbalounge@shubham.co>
2024-09-23 12:33:25 +0530

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  apps: [
+
+    {
+
+      name: 'admin-app',
+
+      script: 'node_modules/next/dist/bin/next',
+
+      args: 'start',
+
+      instances: 1,
+
+      autorestart: true,
+
+      watch: false,
+
+      max_memory_restart: '4G',
+
+      env: {
+
+        NODE_ENV: 'uat',
+
+        PORT: 3300
+
+      },
+
+    },
+
+  ],
+};
