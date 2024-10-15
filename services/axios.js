a416963c5f5f9d22f143e898b0e1671e7555decd
manish <manish.singh@dbalounge.com>
2024-10-15 11:45:29 +0530

    season expire done

--- services/axios.js
+++ services/axios.js
@@ -3,6 +3,8 @@ import Cookies from 'js-cookie';
 
 import store from '@/redux/store';
 import { startLoaderAct, stopLoaderAct } from '@/redux/slice/loader.slice';
+import pageRoutes from '@/utils/page.Routes';
+import { removeUserCookies } from '@/hooks/useRemoveCookies';
 
 const axios = request.create();
 
@@ -28,10 +30,13 @@ axios.interceptors.response.use(
     },
     (error) => {
         store.dispatch(stopLoaderAct())
-        // const status = error?.response?.status || 400
-        // if (status === 401) {
-        // ...
-        // }
+        const status = error?.response?.status || 400
+        if (status === 401) {
+            alert('Session Expired!')
+            removeUserCookies()
+            // pageRoutes.SIGIN_PAGE()
+            window.location.replace(pageRoutes.SING_IN())
+        }
         return Promise.reject(error);
     }
 )
